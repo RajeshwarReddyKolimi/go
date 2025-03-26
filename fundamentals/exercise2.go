0ed9f8c991e6c4882928579b2ffd3765a18824ec
Rajeshwar <kolimirajeshwarreddy@gmail.com>
2025-03-26 10:58:58 +0530

    Implemented Exercise 2

---
+++ fundamentals/exercise2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+func checkEqual(arr1 []int, arr2 []int) bool {
+	for i := 0; i < len(arr1); i++ {
+		if arr1[i] != arr2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func checkSublist(arr1 []int, arr2 []int) bool {
+	for ind := 0; ind <= len(arr2)-len(arr1); ind++ {
+		flag := true
+		for i, j := 0, ind; i < len(arr1) && j < len(arr2); i, j = i+1, j+1 {
+			if arr1[i] != arr2[j] {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			return true
+		}
+	}
+	return false
+}
+
+func Exercise2() {
+	arr1 := []int{1, 1, 2}
+	arr2 := []int{1, 1, 2, 1}
+	l1 := len(arr1)
+	l2 := len(arr2)
+	if l1 == l2 {
+		if checkEqual(arr1, arr2) {
+			fmt.Println("A and B are equal")
+			return
+		}
+	} else if l1 < l2 {
+		if checkSublist(arr1, arr2) {
+			fmt.Println("A is sublist of B")
+			return
+		}
+	} else {
+		if checkSublist(arr2, arr1) {
+			fmt.Println("A is superlist of B")
+			return
+		}
+	}
+	fmt.Println("A and B are unequal")
+}
--- fundamentals/main.go
+++ fundamentals/main.go
@@ -1,36 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var n1 int
-	var n2 int
-	var op string
-	fmt.Println("Enter first number")
-	fmt.Scanln(&n1)
-	fmt.Println("Enter second number")
-	fmt.Scanln(&n2)
-	fmt.Println("Enter operation (+, -, *, /)")
-	fmt.Scanln(&op)
-	switch op {
-	case "+":
-		fmt.Printf("Sum of %v, %v is %v\n", n1, n2, n1+n2)
-	case "-":
-		fmt.Printf("Difference of %v, %v is %v\n", n1, n2, n1-n2)
-	case "*":
-		fmt.Printf("Product of %v, %v is %v\n", n1, n2, n1*n2)
-	case "/":
-		{
-			if n2 == 0 {
-				e := fmt.Errorf("Error: Division by zero")
-				fmt.Println(e)
-			} else {
-				fmt.Printf("Quotient of %v, %v is %v\n", n1, n2, n1/n2)
-			}
-		}
-	default:
-		fmt.Printf("No operator selected\n")
-	}
+	// Exercise1()
+	Exercise2()
 }
