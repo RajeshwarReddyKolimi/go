b58f45a56599b055984320a46370fb934fe4170a
Rajeshwar <kolimirajeshwarreddy@gmail.com>
2025-04-14 11:45:18 +0530

    Refactored calculator implementation with suggested improvements

--- fundamentals/exercise1.go
+++ fundamentals/exercise1.go
@@ -2,9 +2,45 @@ package main
 
 import "fmt"
 
+type Calculator interface {
+	add(a, b float64) float64
+	subtract(a, b float64) float64
+	multiply(a, b float64) float64
+	divide(a, b float64) float64
+}
+
+func add(a, b float64) float64 {
+	sum := a + b
+	fmt.Println("The sum is ", sum)
+	return sum
+}
+
+func subtract(a, b float64) float64 {
+	difference := a - b
+	fmt.Println("The difference is ", difference)
+	return difference
+}
+
+func multiply(a, b float64) float64 {
+	product := a * b
+	fmt.Println("The product is ", product)
+	return product
+}
+
+func divide(a, b float64) float64 {
+	if b == 0 {
+		e := fmt.Errorf("Error: Division by zero")
+		fmt.Println(e)
+		return 0
+	}
+	quotient := a / b
+	fmt.Println("The quotient is ", quotient)
+	return quotient
+}
+
 func Exercise1() {
-	var n1 int
-	var n2 int
+	var n1 float64
+	var n2 float64
 	var op string
 	fmt.Println("Enter first number")
 	fmt.Scanln(&n1)
@@ -14,20 +50,13 @@ func Exercise1() {
 	fmt.Scanln(&op)
 	switch op {
 	case "+":
-		fmt.Printf("Sum of %v, %v is %v\n", n1, n2, n1+n2)
+		add(n1, n2)
 	case "-":
-		fmt.Printf("Difference of %v, %v is %v\n", n1, n2, n1-n2)
+		subtract(n1, n2)
 	case "*":
-		fmt.Printf("Product of %v, %v is %v\n", n1, n2, n1*n2)
+		multiply(n1, n2)
 	case "/":
-		{
-			if n2 == 0 {
-				e := fmt.Errorf("Error: Division by zero")
-				fmt.Println(e)
-			} else {
-				fmt.Printf("Quotient of %v, %v is %v\n", n1, n2, n1/n2)
-			}
-		}
+		divide(n1, n2)
 	default:
 		fmt.Printf("No operator selected\n")
 	}
